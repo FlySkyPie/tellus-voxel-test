b3770622a0c31bf3ff76188cc625a130cb20a854
Wei Ji (FlySkyPie) <c445dj544@gmail.com>
2024-03-24 12:06:34 +0800

    feat: Add wishes

--- src/pages/chunk-editor/chunk-editor.tsx
+++ src/pages/chunk-editor/chunk-editor.tsx
@@ -1,3 +1,15 @@
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import "github-markdown-css/github-markdown.css";
+
+import markdownStr from "./todo.md?raw";
+
 export const ChunkEditor: React.FC = () => {
-  return null;
+  return (
+    <div className="mt-8">
+      <Markdown className="markdown-body" remarkPlugins={[remarkGfm]}>
+        {markdownStr}
+      </Markdown>
+    </div>
+  );
 };
---
+++ src/pages/chunk-editor/todo.md
@@ -0,0 +1,8 @@
+# Todo
+
+"Todo", you known... ( ͡° ͜ʖ ͡°)
+
+- [ ] Edit chunk
+- [ ] Export file
+- [ ] Import file
+- [ ] 3D Chunk Preview
